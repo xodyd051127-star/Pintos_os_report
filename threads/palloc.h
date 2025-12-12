3e69bf1b7b8c980e8fff3fe7961c6ac689078f91
xodyd051127-star <xodyd051127@gmail.com>
2025-12-12 21:08:36 +0900

    Update palloc.h

--- threads/palloc.h
+++ threads/palloc.h
@@ -11,6 +11,22 @@ enum palloc_flags
     PAL_USER = 004    /* User page. */
 };
 
+/* [프로젝트 2: 메모리 할당] 추가 시작 */
+
+/* Contiguous allocation mode selector */
+enum palloc_mode
+{
+    PAL_FIRST_FIT, // 기본 제공 (bitmap_scan을 사용)
+    PAL_NEXT_FIT,  // Next Fit 할당 기법 (bitmap_scan을 시작 인덱스부터 사용)
+    PAL_BEST_FIT,  // Best Fit 할당 기법 (bitmap_scan_best_fit을 사용)
+    PAL_BUDDY      // Buddy System 할당 기법 (별도의 리스트를 사용)
+};
+
+void palloc_set_mode (enum palloc_mode);
+
+/* [프로젝트 2: 메모리 할당] 추가 끝 */
+
+
 void palloc_init (size_t user_page_limit);
 void *palloc_get_page (enum palloc_flags);
 void *palloc_get_multiple (enum palloc_flags, size_t page_cnt);
